6bf370ed00d451f1596a38e6a976bef8828f0e00
Tenhu <tenhu@tenhus-mbp.cs.mum.edu>
2019-04-19 14:41:45 -0500

    Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { MovieComponent } from './movie/movie.component';
+import { ReviewComponent } from './review/review.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    MovieComponent,
+    ReviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
