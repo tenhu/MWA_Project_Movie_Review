af7f4b73d05c9f9fcde50222517a8315d1ee65a7
Tenhu <tenhu@Tenhus-MacBook-Pro.local>
2019-04-23 09:50:11 -0500

    changed auth-jwt.ts and login component
    added navigation when user successfully login

--- MWAAngular/src/app/auth/auth-jwt.ts
+++ MWAAngular/src/app/auth/auth-jwt.ts
@@ -1,9 +1,19 @@
 import { JwtModule } from '@auth0/angular-jwt';
 
+// export const JwtConfig = JwtModule.forRoot({
+//   config: {
+//     tokenGetter: () => {
+//       return localStorage.getItem('access_token');
+//     }
+//   }
+// });
+
+export function jwtTokenGetter() {
+  return localStorage.getItem('access_token');
+}
+
 export const JwtConfig = JwtModule.forRoot({
   config: {
-    tokenGetter: () => {
-      return localStorage.getItem('access_token');
-    }
+    tokenGetter: jwtTokenGetter
   }
-});
+})
