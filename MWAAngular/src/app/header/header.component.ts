bb7ad4ba241b9fd2560fa6ea38d16da0530dadbc
Tenhu <tenhu@Tenhus-MacBook-Pro.local>
2019-04-22 22:51:56 -0500

    Merged login status to header
    patch on login page

--- MWAAngular/src/app/header/header.component.ts
+++ MWAAngular/src/app/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { authStore } from '../auth/auth-store';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styles: []
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userinfo;unsubscribe;
+
+  constructor() {     
+  }
 
   ngOnInit() {
+    this.unsubscribe = authStore.subscribe(()=>{this.userinfo = authStore.getState().userinfo});
+    this.userinfo = authStore.getState().userinfo;
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe();
   }
 
 }
